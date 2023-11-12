9b2a8a6abbc1c75488b6a2d41e909d4b739d942c
akmalviya03 <abhishakkrmalviya@gmail.com>
2023-11-12 22:22:01 +0530

    custom painter added to gravity

--- lib/main.dart
+++ lib/main.dart
@@ -23,22 +23,15 @@ class GravitySimulationDemo extends StatefulWidget {
   State<GravitySimulationDemo> createState() => _GravitySimulationDemoState();
 }
 
-class _GravitySimulationDemoState extends State<GravitySimulationDemo>
-    with SingleTickerProviderStateMixin {
+class _GravitySimulationDemoState extends State<GravitySimulationDemo> with SingleTickerProviderStateMixin {
   late AnimationController _controller;
-  late Animation<Alignment> _animation;
+  late double height;
+  late double width;
 
   @override
   void initState() {
     super.initState();
     _controller = AnimationController.unbounded(vsync: this);
-    _animation = _controller.drive(AlignmentTween(
-        begin: const Alignment(
-          0,
-          -1,
-        ),
-        end: const Alignment(0, 1)));
-
     _controller.addListener(() {
       if (kDebugMode) {
         print(_controller.value);
@@ -53,41 +46,63 @@ class _GravitySimulationDemoState extends State<GravitySimulationDemo>
   }
 
   @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    height = (MediaQuery.of(context).size.height -
+        (MediaQuery.maybeViewPaddingOf(context)?.vertical ?? 0));
+    width = MediaQuery.sizeOf(context).width;
+    if (kDebugMode) {
+      print(MediaQuery.of(context).size);
+      print(MediaQuery.maybeViewPaddingOf(context)?.vertical);
+      print(MediaQuery.of(context).devicePixelRatio);
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Center(
-          child: Column(
-            children: [
-              Expanded(
-                child: AnimatedBuilder(
-                    animation: _animation,
-                    child: Container(
-                      color: Colors.black,
-                      height: 10,
-                      width: 10,
-                    ),
-                    builder: (context, child) {
-                      return Align(
-                        alignment: _animation.value,
-                        child: child,
-                      );
-                    }),
-              ),
-              ElevatedButton(
-                  onPressed: () {
-                    _controller.animateWith(GravitySimulation(
-                      0.01, //Acceleration in pixels
-                      0, // Initial Position in pixels
-                      1, // Final Position in pixels
-                      0.01, // Velocity in pixels
-                    ));
-                  },
-                  child: const Text('Play Gravity Simulation'))
-            ],
+        child: Align(
+          alignment: Alignment.topCenter,
+          child: CustomPaint(
+            painter: BoxPainter(animation: _controller),
+            size: Size(width, height),
           ),
         ),
       ),
+      floatingActionButton: FloatingActionButton.extended(
+        isExtended: true,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+        onPressed: () {
+          _controller.animateWith(GravitySimulation(
+            30, //Acceleration in pixels
+            0, // Initial Position in pixels
+            height-20, // Final Position in pixels
+            20, // Velocity in pixels
+          ));
+        },
+        label: const Text('Play Simulation'),
+      ),
     );
+  }
+}
+
+class BoxPainter extends CustomPainter {
+  final Animation animation;
+  BoxPainter({required this.animation}) : super(repaint: animation);
+  @override
+  void paint(Canvas canvas, Size size) {
+    canvas.translate(0, 10);
+    canvas.drawRect(
+        Rect.fromCenter(
+            center: Offset(size.width / 2, animation.value),
+            width: 20,
+            height: 20),
+        Paint()..color = Colors.green);
+  }
+
+  @override
+  bool shouldRepaint(BoxPainter oldDelegate) {
+    return true;
   }
 }
